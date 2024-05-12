90395605f2e6be4cbe7e7663799aa608da262be3
Mahesh Navghane <mnavghane66.gmail.com>
2024-05-12 18:09:08 +0530

    python code is updated

--- demo.py
+++ demo.py
@@ -1,12 +1,5 @@
-import json
-
 def lambda_handler(event, context):
-    """
-    This is a basic example Lambda function. Replace with your actual logic.
-    """
-    message = "Hello from your deployed Lambda function!"
-
     return {
-        "statusCode": 200,
-        "body": json.dumps(message)
+        'statusCode': 200,
+        'body': 'Hello, World!'
     }
