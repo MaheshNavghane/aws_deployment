d65ffc42c90e35eef8255917bc65c1f606c6a556
Mahesh Navghane <mnavghane66.gmail.com>
2024-05-12 17:30:07 +0530

    Terraform folder is ignored and some updates are added in gh workflows

--- demo.py
+++ demo.py
@@ -1,5 +1,12 @@
+import json
+
 def lambda_handler(event, context):
+    """
+    This is a basic example Lambda function. Replace with your actual logic.
+    """
+    message = "Hello from your deployed Lambda function!"
+
     return {
-        'statusCode': 200,
-        'body': 'Hello, World!'
+        "statusCode": 200,
+        "body": json.dumps(message)
     }
