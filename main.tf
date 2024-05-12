5f7508841ee0488c3e6ce9894c70f6d4c89bb61a
Mahesh Navghane <mnavghane66.gmail.com>
2024-05-12 12:44:53 +0530

    The pythod and terraform is created

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "your_region"
+}
+
+resource "aws_lambda_function" "hello_world_lambda" {
+  filename      = "lambda_function.zip"
+  function_name = "hello-world-lambda"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "hello_world_lambda.lambda_handler"
+  runtime       = "python3.8"
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+  
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
